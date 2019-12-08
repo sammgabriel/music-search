a7b6c0ae0c620698c82a2e2085a4dff789eb64c9
Sam Gabriel <sgabriel2@mail.greenriver.edu>
2019-12-07 19:07:03 -0800

    Users can now search by artist and song name

---
+++ MusicSearch/MusicSearch/wwwroot/js/music-js.js
@@ -0,0 +1,21 @@
+﻿
+$("#song-form").hide();
+
+$("#by-name").on("click", function () {
+
+    $("#song-form").toggle();
+});
+
+$("#submit").on("click", function () {
+
+    var artist = $("#artist").val();
+    var songName = $("#song").val();
+    var url = "https://api.lyrics.ovh/v1/" + artist + "/" + songName;
+
+    $.getJSON(url, function (result) {
+
+        $("#name-lyrics").append("<h2>" + "Lyrics" + "</h2>");
+
+        $("#name-lyrics").append(result.lyrics);
+    });
+});
