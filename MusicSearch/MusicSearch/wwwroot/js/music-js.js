69be6084647f16a73899c8e2dbe4288ca7608818
Sam Gabriel <sgabriel2@mail.greenriver.edu>
2019-12-09 22:41:42 -0800

    Added citations and edited styling

--- MusicSearch/MusicSearch/wwwroot/js/music-js.js
+++ MusicSearch/MusicSearch/wwwroot/js/music-js.js
@@ -1,4 +1,14 @@
-﻿
+﻿/*
+ *
+ * References:
+ *
+ * https://stackoverflow.com/questions/554273/changing-the-image-source-using-jquery
+ * https://stackoverflow.com/questions/1625865/replace-anchor-text-with-jquery
+ * https://stackoverflow.com/questions/3039310/how-do-i-clear-the-text-contents-of-a-div-in-javascript
+ * https://api.jquery.com/jquery.each/
+ * 
+*/
+
 $("#song-form").hide();
 
 $("#by-name").on("click", function () {
@@ -42,6 +52,8 @@ $("#submit").on("click", function () {
 
         let lines = lyricLines.split("\n");
 
+        // Referred to the jQuery API regarding the syntax for a foreach loop.
+
         $.each(lines, function (index, line) {
 
             $("#name-lyrics").append(line + "<br />");
@@ -51,11 +63,18 @@ $("#submit").on("click", function () {
 
             if ((artist == info.artist) && (songName == info.song)) {
 
+                // Referred to StackOverflow regarding how to change the source
+                // attribute of an image tag.
+
                 $("#cover").attr("src", info.img);
                 $("#song-info").append("<strong>Album: </strong>" + info.album + "<br / >");
                 $("#song-info").append("<strong>Genre: </strong>" + info.genre + "<br / >");
                 $("#audio").attr("href", info.url);
                 $("#audio").attr("target", "_blank");
+
+                // Referred to StackOVerflow regarding how to change anchor tag
+                // text.
+
                 $("#audio").text("Listen To This Song");
 
             } 
@@ -64,6 +83,9 @@ $("#submit").on("click", function () {
 });
 
 $("#clear").on("click", function () {
+
+    // Referred to StackOverflow regarding how to clear text contents of
+    // an element.
 
     $("#name-lyrics").empty();
     $("#song-info").empty();
